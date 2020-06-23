490183d7e96e7a23287b0fb9815fea3a292e9dd7
Seth <neaseth@deloitte.com>
2020-06-22 22:54:16 -0400

    fixing caps config

--- components/Generate.js
+++ components/Generate.js
@@ -16,9 +16,9 @@ export default function Generate(props) {
       <div className="top-sec">
         <h2>Create</h2>
         {hash ? (
-          <div className="token-container" onClick={handleCopyToken}>
+          <button className="token-btn" onClick={handleCopyToken}>
             <span className="token">{hash}</span>
-          </div>
+          </button>
         ) : null}
       </div>
       <MainTextArea
@@ -36,6 +36,7 @@ export default function Generate(props) {
           setHash(sha256(entry));
         }}
         contents="Generate Token"
+        isDisabled={!entry.length > 0}
       />
 
       <style jsx>{`
@@ -51,7 +52,7 @@ export default function Generate(props) {
           align-items: center;
         }
 
-        .token-container {
+        .token-btn {
           cursor: pointer;
           width: 60%;
           text-overflow: ellipsis;
@@ -60,10 +61,16 @@ export default function Generate(props) {
           background-color: whitesmoke;
           padding: 5px;
           border-radius: 0.25rem;
+          box-shadow: 2px 2px 5px rgba(150, 176, 228, 0.6);
         }
 
-        .token-container:active {
+        .token-btn:focus {
+          outline: none;
+        }
+
+        .token-btn:active {
           background-color: #f5f5f500;
+          box-shadow: 2px 2px 2px rgba(150, 176, 228, 0.6);
         }
       `}</style>
     </div>
