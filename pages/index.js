c35333bd45bf57d96f4115d8714093194e9cebd4
Neall Seth <nseth@vt.edu>
2020-07-05 22:01:46 -0400

    fixing lowercase Layout causing build failure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Generate from "../components/Generate";
 import Validate from "../components/Validate";
 
